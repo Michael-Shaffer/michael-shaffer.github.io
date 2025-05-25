b71411b7090c4cbb617cfa78a2e97f60a08d4e50
Michael Shaffer <75963180+Michael-Shaffer@users.noreply.github.com>
2025-05-24 22:11:55 -0400

    dot trail

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const dot = document.getElementById('movable-dot')
 document.addEventListener('mousemove', function(even) {
-	dot.style.left = (event.clientX - 10) + 'px';
-	dot.style.top = (event.clientY - 10) + 'px';
-})
+	dot.style.left = event.clientX + 'px';
+	dot.style.top = event.clientY + 'px';
+	const trail = document.createElement('div');
+	trail.className = 'trail';
+	trail.style.left = (event.clientX - 4) + 'px'
+	trail.style.top = (event.clientY - 4) + 'px'
+	document.body.appendChild(trail);
+	setTimeout(() => {
+		trail.remove();
+	}, 500);
+});
